6bebcafe59b7bbd6f1aea7f8bd647f1bef896d03
Mike Fowler <mike@mikefowler.me>
2012-06-13 20:11:44 -0400

    Added fonts_dir to Compass config

--- config.rb
+++ config.rb
@@ -5,6 +5,7 @@ css_dir = "source/css"
 sass_dir = "sass"
 images_dir = "source/images"
 javascripts_dir = "source/js"
+fonts_dir = "source/fonts"
 output_style = :compressed # or :expanded ?
 relative_assets = true
 line_comments = false
